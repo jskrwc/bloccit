77787b2a8ee7c958373ee9e2039b6ef84d1b9890
Jim Knopf <jsk4@mac.com>
2018-03-10 17:36:11 -0800

    Checkpoint 18 - Rails:CRUD completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,35 @@
 class PostsController < ApplicationController
   def index
-
     @posts = Post.all
   end
 
   def show
+    #find post that corresponds to id in the params passed to show, assign it to @post
+     @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+    # call Post.new to create new instance of Post
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    # if successfully save post to database, display success message
+    if @post.save
+      # assign value to flash[:notice]
+      flash[:notice] = "Post was saved."
+      redirect_to @post     #directs user to post show view
+    else
+      # if unsuccessfully save, display error message and render new view again
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
 
   def edit
   end
