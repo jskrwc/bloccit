7e3007b2b8e2505cf039aaf436d4557d36d58350
Jim Knopf <jsk4@mac.com>
2018-03-12 17:40:25 -0700

    Checkpoint 19 - Rails: More CRUD completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -53,6 +53,74 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  # Checkpoint 19
+
+  describe "GET edit" do
+   it "returns http success" do
+     get :edit, params: { id: my_post.id }
+     expect(response).to have_http_status(:success)
+   end
+
+   it "renders the #edit view" do
+     get :edit, params: { id: my_post.id }
+# expect edit view to render when edit a post
+     expect(response).to render_template :edit
+   end
+
+# test that edit assigns correct post to be upddated to @post
+   it "assigns post to be updated to @post" do
+     get :edit, params: { id: my_post.id }
+
+     post_instance = assigns(:post)
+
+     expect(post_instance.id).to eq my_post.id
+     expect(post_instance.title).to eq my_post.title
+     expect(post_instance.body).to eq my_post.body
+   end
+ end
+
+ describe "PUT update" do
+   it "updates post with expected attributes" do
+     new_title = RandomData.random_sentence
+     new_body = RandomData.random_paragraph
+
+     put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+
+# test that @post was updated with title and body passed to update, and @post's id was not changed
+     updated_post = assigns(:post)
+     expect(updated_post.id).to eq my_post.id
+     expect(updated_post.title).to eq new_title
+     expect(updated_post.body).to eq new_body
+   end
+
+   it "redirects to the updated post" do
+     new_title = RandomData.random_sentence
+     new_body = RandomData.random_paragraph
+
+# expect to be redirected to post show view after update
+     put :update, params: { id: my_post.id, post: {title: new_title, body: new_body } }
+     expect(response).to redirect_to my_post
+   end
+ end
+
+ describe "DELETE destroy" do
+   it "deletes the post" do
+     delete :destroy, params: { id: my_post.id }
+# search db for a post with id = my_post_id...returns array
+# assign size of array to count, then expect count to =0  (i.e. no matchin post after destroy called)
+     count = Post.where({id: my_post.id}).size
+     expect(count).to eq 0
+   end
+
+   it "redirects to posts index" do
+     delete :destroy, params: { id: my_post.id }
+# expect redirect to posts index view after deleting a post
+     expect(response).to redirect_to posts_path
+   end
+ end
+
+
+
   describe "POST create" do
   # when create invoked, new object is persisted to the database
     it "increases the number of Post by 1" do
