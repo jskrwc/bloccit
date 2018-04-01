8098365c29c04909ea071db9d7c0d4bce5f85b55
Jim Knopf <jsk4@mac.com>
2018-03-31 22:08:46 -0700

    Checkpoint 22- Rails: validations completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def form_group_tag(errors, &block)
+    # CP 22  The & turns the block into a Proc -- a block that can be reused like a variable.
+    css_class = 'form-group'
+    css_class << ' has-error' if errors.any?
+
+    #  The content_tag helper method method -- takes a symbol argument, a block, and an options hash. It then creates the symbol-specified HTML tag with the block contents, and if specified, the options.
+    
+    content_tag :div, capture(&block), class: css_class
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,6 +14,16 @@ RSpec.describe Post, type: :model do
 
   it { is_expected.to belong_to(:topic) }
 
+  # CP 22
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
+
+
   # test has attributes title and body
   describe "attributes" do
    it "has title and body attributes" do
