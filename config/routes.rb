799a454dbe688253edce4a6db708eaf54920c762
Jim Knopf <jsk4@mac.com>
2018-04-02 15:26:41 -0700

    Checkpoint 23 - Rails: Authentication - User Model completed

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+  # nb only prevents rails from creating unnecessary routes
+  resources :users, only: [:new, :create]
+
   # get 'welcome/index'
   # get 'welcome/about'
 
