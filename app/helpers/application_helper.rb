f14d771510ee0dfc2adf11bd30a2dc9ffdc5c573
Jim Knopf <jsk4@mac.com>
2018-04-07 11:34:56 -0700

    Assignment 25 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,14 @@ module ApplicationHelper
     css_class << ' has-error' if errors.any?
 
     #  The content_tag helper method method -- takes a symbol argument, a block, and an options hash. It then creates the symbol-specified HTML tag with the block contents, and if specified, the options.
-    
+
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
+
+
 end
